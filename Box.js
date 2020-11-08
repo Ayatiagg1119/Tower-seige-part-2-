b1c935a0bd9e9df7b0b28e0851c5317fc7d49793
Ayatiagg1119 <68820262+Ayatiagg1119@users.noreply.github.com>
2020-11-08 11:05:41 -0500

    Add files via upload

---
+++ Box.js
@@ -0,0 +1,22 @@
+class Box extends BaseClass {
+  constructor(x, y, width, height){
+    super(x,y,width,height);
+    this.image = loadImage("sprites/wood1.png");
+  }
+function score(){
+  blocks1.score();
+  blocks2.score();
+  blocks3.score();
+  blocks4.score();
+  blocks5.score();
+  blocks6.score();
+  blocks7.score();
+  blocks8.score();
+  blocks9.score();
+}
+score(){
+  if(this.visibility<0 && this.visibility>-105){
+    score++;
+    }
+  }
+};
